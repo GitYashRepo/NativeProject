d40d54d09aeb87003eabc3b263e0352b4e69c556
yashrepogit <yas@cloudify.biz>
2025-09-09 00:24:02 +0530

    added movile app

--- demo/app/_layout.tsx
+++ demo/app/_layout.tsx
@@ -1,5 +1,23 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+// import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import * as SecureStore from "expo-secure-store";
+import SafeScreen from "@/components/SafeScreen";
+
+const tokenCache = {
+  getToken: (key: any) => SecureStore.getItemAsync(key),
+  saveToken: (key: any, value: string) => SecureStore.setItemAsync(key, value),
+};
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider
+        publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+        tokenCache={tokenCache}
+    >
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
+    </ClerkProvider>
+  );
 }
